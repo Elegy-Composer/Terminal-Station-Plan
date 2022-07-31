4cf3f2d62ce25a7b4a79375ce938b6811c0c54cd
Jerry Wu <a91082900@gmail.com>
2022-07-31 19:19:22 +0800

    add TileManager to change tiles and instantiate gameObject

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    public Tilemap map;
+    public void SetTile(Vector3Int[] positions, TileBase[] tiles)
+    {
+        map.SetTiles(positions, tiles);
+    }
+
+    public void SetTile(Vector3Int[] positions, TileBase tile)
+    {
+        TileBase[] tiles = new TileBase[positions.Length];
+        
+        for(int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i] = tile;
+        }
+        map.SetTiles(positions, tiles);
+    }
+
+    public void SetTile(Vector3Int position, TileBase tile)
+    {
+        map.SetTile(position, tile);
+    }
+
+    public void SetTile(Vector3Int position, TileBase tile, GameObject gameObject)
+    {
+        map.SetTile(position, tile);
+        Instantiate(gameObject, map.CellToWorld(position), Quaternion.identity);
+    }
+}
