e033e75981d9d4b601fa93377ea794b2f45c580b
hankyen0315 <hank_yen@yahoo.com>
2022-08-14 00:28:13 +0800

    Removal Button can remove the poles on the ground

---
+++ Assets/Objective.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MapObject.Interactable;
+
+
+public class Objective : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+
+    }
+    public bool CheckInteractionEnd()
+    {
+        return true;
+    }
+
+
+}
